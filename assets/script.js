5a6fd9f867d6f26db811246832fa1b84769e97aa
April H <aprilhunt00.ah@gmail.com>
2024-02-14 05:38:02 -0500

    js updated with api

--- assets/script.js
+++ assets/script.js
@@ -5,19 +5,15 @@ function initPage() {
     const historyEl = document.getElementById("history");
     let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
     
+var apiKey= "411f4af06666b68c0b3ffe9e5b572d71";
     
-    var apiKey= "411f4af06666b68c0b3ffe9e5b572d71";
-    
-    
-    function getWeather(cityName) {
+ function getWeather(cityName) {
     const queryURL = "";
     }
     
-    
-    function renderSearchHistory() {
+function renderSearchHistory() {
     historyEl.innerHTML = searchHistory.map(city => `<input type="text" readonly class="form-control d-block bg-white" value="${city}" />`).join('');
     }
-    
     
     searchEl.addEventListener("click", () => {
     const searchTerm = cityEl.value;
@@ -27,34 +23,23 @@ function initPage() {
     renderSearchHistory();
     });
     
-    
     clearEl.addEventListener("click", () => {
     localStorage.clear();
     searchHistory = [];
     renderSearchHistory();
     });
     
-    
     renderSearchHistory();
     }
-    
-    
     initPage();
     
-    
-    
-    
-    
-    
-    
-    
-    function pullStorage(){
+ function pullStorage(){
     $('#list-group').empty();
-    
     
     for (var i = 0; i < localStorage.length; i++) {
     var city = localStorage.getItem("cities" + i);
     var cityEl;
+
     // Set to lastCity
     if (currentCity === "") {
     currentCity = lastCity;
@@ -64,17 +49,14 @@ function initPage() {
     } else {
     cityEl = `<button type="button" class="list-group-item list-group-item-action">${city}</button></li>`;
     }
-    // Append city to page
+
+ // Append city to page
     $('#list-group').prepend(cityEl);
     }
     if (localStorage.length > 0) {
     $('#clear-storage').html($('<a id="clear-storage" href="#">clear</a>'));
     } else {
     $('#clear-storage').html('');
-    }
-    }
-    
-    
-    
-    
-    pullStorage();
+    } }
+
+ pullStorage();
